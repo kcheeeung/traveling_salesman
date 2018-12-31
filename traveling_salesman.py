c06d723302e1858d410401408f2769792f72c9c4
k.cheung@berkeley.edu <k.cheung@berkeley.edu>
2018-12-31 00:14:19 -0800

    algorithm overhaul

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -14,34 +14,41 @@ class TravelingSalesman_GeneticAlgorithm():
         """
         self.points_to_visit  = points_to_visit
         self.index_home       = index_home
-        self.population_size  = 4
-        self.elite_size       = 2
+        self.population_size  = 24
+        self.elite_size       = 3
         self.crossover_chance = 0.9
-        self.mutation_chance  = 0.2
-
+        self.mutation_chance  = 0.1
+        self.sequence_length  = None
+        
         self.population = []
-        self.parents    = []
-        self.elite_pop  = []
+        self.best_gene  = None
+        self.best_dist  = None
 
         self.initialize_population()
+        self.set_best_gene()
+        print("Best relative value:", self.best_dist)
 
     def run_algorithm(self, generations):
         for _ in range(generations):
             self.selection()
-            # self.crossover()
-            # self.mutate()
+            self.crossover()
+            self.mutate()
+            self.compare_best_gene()
+        print("Best relative value:", self.best_dist)
 
-    def calculate_distance(self, gene):
-        dist = 0
-        for i in range(len(gene) - 1):
-            x = gene[i][0]-gene[i+1][0]
-            y = gene[i][1]-gene[i+1][1]
-            dist += (x*x + y*y)**0.5
-        return dist
+    # def calculate_distance(self, gene):
+    #     dist = 0
+    #     for i in range(self.sequence_length-1):
+    #         # dist += np.linalg.norm(gene[i]-gene[i+1])
+    #         x = gene[i][0]-gene[i+1][0]
+    #         y = gene[i][1]-gene[i+1][1]
+    #         dist += (x*x + y*y)**0.5
+    #     return dist
 
     def initialize_population(self):
         home = self.points_to_visit[self.index_home]
         gene_sequence = self.points_to_visit.tolist()
+        self.sequence_length = len(gene_sequence)
         del gene_sequence[self.index_home]
 
         for _ in range(self.population_size):
@@ -52,8 +59,36 @@ class TravelingSalesman_GeneticAlgorithm():
             # Record data
             self.population.append(gene)
 
-        # print(self.population)
-        # print("Pop size:", len(self.population))
+    def set_best_gene(self):
+        distance = []
+        for gene in self.population:
+            dist = 0
+            for i in range(self.sequence_length-1):
+                x = gene[i][0]-gene[i+1][0]
+                y = gene[i][1]-gene[i+1][1]
+                dist += x*x + y*y
+            distance.append(dist)
+        best_dist  = min(distance)
+        best_index = distance.index(best_dist)
+        # Record best gene and distance
+        self.best_dist = best_dist
+        self.best_gene = self.population[best_index]
+
+    def compare_best_gene(self):
+        distance = []
+        for gene in self.population:
+            dist = 0
+            for i in range(self.sequence_length-1):
+                x = gene[i][0]-gene[i+1][0]
+                y = gene[i][1]-gene[i+1][1]
+                dist += x*x + y*y
+            distance.append(dist)
+        current_best_dist  = min(distance)
+        current_best_index = distance.index(current_best_dist)
+        # Compare current gen with all time best
+        if current_best_dist < self.best_dist:
+            self.best_dist = current_best_dist
+            self.best_gene = self.population[current_best_index]
 
     def selection(self):
         """
@@ -64,7 +99,7 @@ class TravelingSalesman_GeneticAlgorithm():
         total_dist = 0
         for gene in self.population:
             dist = 0
-            for i in range(len(gene)-1):
+            for i in range(self.sequence_length-1):
                 x = gene[i][0]-gene[i+1][0]
                 y = gene[i][1]-gene[i+1][1]
                 dist += x*x + y*y
@@ -82,128 +117,119 @@ class TravelingSalesman_GeneticAlgorithm():
             chance += fitness[i]
             roulette.append(chance)
 
-        print(distance)
-        print(fitness)
-        print(roulette)
+        # Add all time best
+        temp_parents = []
+        temp_parents.append(self.best_gene)
 
-        # Locate best n elites
-        del self.elite_pop[0:]
-        temp_elite_list = []
+        # Locate current best n elites
         for _ in range(self.elite_size):
             # Find index of elite individual
             max_elite_fit = max(fitness)
-            elite_index = fitness.index(max_elite_fit)
-            # Clone elite
-            temp_elite_list.append(self.population[elite_index])
+            elite_index   = fitness.index(max_elite_fit)
+            # Clone elite (creates 1 original + 2 mutated per n elites)
+            elite_individual = self.population[elite_index]
+            temp_parents.append(elite_individual)
+            # Mutation
+            temp_elite = elite_individual.tolist()
+            seq_len = len(temp_elite)-1
+            while True:
+                mut_0, mut_1 = random.randint(1, seq_len), random.randint(1, seq_len)
+                # At least 2 pieces long (adding +1)
+                if mut_0+1 < mut_1:
+                    break
+            # Inversion operator
+            gene_ax = temp_elite[0:mut_0]
+            gene_bx = temp_elite[mut_1:]
+            segment = temp_elite[mut_0:mut_1]
+            segment = segment[::-1]
+            temp_parents.append(np.array(gene_ax + segment + gene_bx))
+            # Shift operator
+            shift_gene = temp_elite[1:]
+            shift = random.randint(1, len(shift_gene)-1)
+            temp_parents.append(np.array([temp_elite[0]]+shift_gene[shift:]+shift_gene[:shift]))
+            # "Remove elite" to find next n elite
             fitness[elite_index] = -999
 
-        print('helllllllllllllll')
-        print(distance)
-        print(fitness)
-        print(roulette)
-
-        # Roulette selection of parents
-        while True:
-            roll_a, roll_b = random.random(), random.random()
-            parent_a_index, parent_b_index = 0, 0
-            for element in roulette:
-                if roll_a > element:
-                    parent_a_index += 1
-                else:
+        # Roulette selection for remaining spots
+        queue_indices = []
+        for i in range(self.population_size - len(temp_parents)):
+            while True:
+                roll = random.random()
+                parent_index = 0
+                for element in roulette:
+                    if roll > element:
+                        parent_index += 1
+                    else:
+                        break
+                if parent_index not in queue_indices:
+                    queue_indices.append(parent_index)
                     break
-            for element in roulette:
-                if roll_b > element:
-                    parent_b_index += 1
-                else:
-                    break
-            if parent_a_index != parent_b_index:
-                break
+            temp_parents.append(self.population[queue_indices[i]])
 
-        # Clone 2 selected parents
-        self.parents.append(self.population[parent_a_index])
-        self.parents.append(self.population[parent_b_index])
-        # Clone elites
-        self.elite_pop += temp_elite_list
-
+        # Clear previous population
+        del self.population[0:]
+        self.population += temp_parents
+        # print(len(self.population))
         # print("Average dist", total_dist/self.population_size)
-        # print("Distance:", distance)
-        # print("Fitness :", fitness)
-        # print("Roulette:", roulette)
-        # print("Elite 0 :", self.elite_pop[0])
-        print("Elite dist:", self.calculate_distance(self.elite_pop[0]))
 
     def crossover(self):
         """
         Crossover
         """
-        # Clear previous population
-        del self.population[0:]
         # Crossover genes
-        a = self.parents[0].tolist()
-        b = self.parents[1].tolist()
-        for _ in range(int((self.population_size-self.elite_size)/2)):
-            if random.random() <= self.crossover_chance:
-                seq_len = len(a)-1
-                while True:
-                    cross_0, cross_1 = random.randint(1, seq_len), random.randint(1, seq_len)
-                    if cross_0 < cross_1:
-                        break
-                # Partially-Mapped Crossover (PMX)
-                # child_a
-                gene_ax = a[0:cross_0]
-                gene_ay = a[cross_1:]
-                gene_bx = [segment_b for segment_b in b if segment_b not in gene_ax+gene_ay]
-                child_a = np.array(gene_ax + gene_bx + gene_ay)
-                self.population.append(child_a)
-                # child_b
-                gene_bj = b[0:cross_0] 
-                gene_bk = b[cross_1:]
-                gene_aj = [segment_a for segment_a in a if segment_a not in gene_bj+gene_bk]
-                child_b = np.array(gene_bj + gene_aj + gene_bk)
-                self.population.append(child_b)
-            else:
-                temp_a, temp_b = a[1:], b[1:]
-                random.shuffle(temp_a)
-                random.shuffle(temp_b)
-                self.population.append(np.array([a[0]] + temp_a))
-                self.population.append(np.array([b[0]] + temp_b))
+        queue_indices = [i for i in range(len(self.population)) if random.random() < self.crossover_chance]
+        random.shuffle(queue_indices)
+        for i in range(0, len(queue_indices)-1, 2):
+            a = self.population[queue_indices[i]].tolist()
+            b = self.population[queue_indices[i+1]].tolist()
+            seq_len = self.sequence_length-1
+            while True:
+                cross_0, cross_1 = random.randint(1, seq_len), random.randint(1, seq_len)
+                if cross_0+1 < cross_1:
+                    break
+            # Partially-Mapped Crossover (PMX)
+            # child_a
+            gene_ax = a[0:cross_0]
+            gene_ay = a[cross_1:]
+            gene_bx = [segment_b for segment_b in b if segment_b not in gene_ax+gene_ay]
+            self.population[i]   = np.array(gene_ax + gene_bx + gene_ay)
+            # # child_b
+            gene_bj = b[0:cross_0] 
+            gene_bk = b[cross_1:]
+            gene_aj = [segment_a for segment_a in a if segment_a not in gene_bj+gene_bk]
+            self.population[i+1] = np.array(gene_bj + gene_aj + gene_bk)
+            
+            # print(a)
+            # print(self.population[i].tolist())
+            # print(b)
+            # print(self.population[i+1].tolist())
+            # print("\n")
 
     def mutate(self):
         """
         Mutate progeny
         """
-        if random.random() < self.mutation_chance:
-            seq_len = len(self.population[0])-1
-            for i in range(len(self.population)):
+        for i in range(len(self.population)):
+            if random.random() < self.mutation_chance:
                 temp_gene = self.population[i].tolist()
                 while True:
-                    mut_0, mut_1 = random.randint(1, seq_len), random.randint(1, seq_len)
+                    mut_0, mut_1 = random.randint(1, self.sequence_length), random.randint(1, self.sequence_length)
                     # Mutation at least 2 pieces long (adding +1)
                     if mut_0+1 < mut_1:
                         break
-                # # Segment shuffle
-                # gene_ax = temp_gene[0:mut_0]
-                # gene_bx = temp_gene[mut_1:]
-                # segment = temp_gene[mut_0:mut_1]
-                # random.shuffle(segment)
-                # # Mutate gene
-                # self.population[i] = np.array(gene_ax + segment + gene_bx)
-                roll = random.random()
-                if roll < 0.5:
-                    # inversion operator
+                # Mutate
+                if random.random() < 0.5:
+                    # Inversion operator
                     gene_ax = temp_gene[0:mut_0]
                     gene_bx = temp_gene[mut_1:]
                     segment = temp_gene[mut_0:mut_1]
                     segment = segment[::-1]
-                    # Mutate gene
                     self.population[i] = np.array(gene_ax + segment + gene_bx)
                 else:
-                    # shift operator
-                    shift = random.randint(1, len(temp_gene))
-                    self.population[i] = np.array([temp_gene[0]] + temp_gene[shift:] + temp_gene[:shift])
-
-        # Add elite individuals back to population
-        self.population += self.elite_pop
+                    # Shift operator
+                    shift_gene = temp_gene[1:]
+                    shift = random.randint(1, len(shift_gene)-1)
+                    self.population[i] = np.array([temp_gene[0]] + shift_gene[shift:] + shift_gene[:shift])
 
 def initalize_binary_ndarray():
     """
@@ -215,16 +241,16 @@ def initalize_binary_ndarray():
     [[row, column]], [row, column], ...]
     """
     # Test matrix
-    matrix = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
+    matrix = np.array([[0, 0, 0, 0, 1, 0, 0, 1, 0],
                        [0, 1, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 1, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [1, 0, 0, 0, 1, 0, 1, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 1, 0, 0, 0, 0, 0, 1, 0],
-                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 1, 1, 0, 0, 1, 0, 0]])
+                       [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 0, 0, 0, 0, 1, 0],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 0, 1, 1, 0, 0, 1, 0, 0]])
 
     # rows = 10
     # cols = 10
@@ -263,9 +289,9 @@ def main():
     start_index = starting_point(points_to_visit)
 
     TSP_GA = TravelingSalesman_GeneticAlgorithm(points_to_visit, start_index)
-    TSP_GA.run_algorithm(1)
+    TSP_GA.run_algorithm(1000)
 
 if __name__ == '__main__':
     start_time = timer()
     main()
-    print("Finished in: {} secs".format(round(timer() - start_time, 7)))
+    print("Finished in: {} secs".format(round(timer() - start_time, 5)))
