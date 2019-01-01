d4bc32ff26af6426979de7e99c32bd3928ac6f98
k.cheung@berkeley.edu <k.cheung@berkeley.edu>
2018-12-31 18:09:22 -0800

    improvements, comments, and images

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -9,9 +9,17 @@ class TravelingSalesman_GeneticAlgorithm():
     """
     def __init__(self, points_to_visit, index_home):
         """
-        points_to_visit, ndarray of points to visit
-        index_home,      index of starting point in points_to_visit
-        population_size, the population size
+        self.points_to_visit   list of points to visit
+        self.index_home        index of starting point in points_to_visit
+        self.population_size   the population size
+        self.elite_size        # of elites to carry to next generation x 3
+        self.crossover_chance  % chance to crossover
+        self.mutation_chance   % chance to mutate progeny
+
+        self.history           Records all time best_dist over generation
+        self.population        Holds population of current gen
+        self.best_gene         Best path/sequence of individual
+        self.best_dist         Best distance/value of indvidual
         """
         self.points_to_visit  = points_to_visit
         self.index_home       = index_home
@@ -21,51 +29,53 @@ class TravelingSalesman_GeneticAlgorithm():
         self.mutation_chance  = 0.1
         self.sequence_length  = None
         
+        self.history    = []
         self.population = []
         self.best_gene  = None
         self.best_dist  = None
-        self.history    = []
 
         self.initialize_population()
         self.set_best_gene()
-        print("Best relative value:", self.best_dist)
+        print("Start best value: {}".format(self.best_dist))
 
     def run_algorithm(self, generations):
+        """Runs genetic algorithm for # generations"""
         for _ in range(generations):
             self.selection()
             self.crossover()
             self.mutate()
             self.compare_best_gene()
-        print("Best relative value:", self.best_dist)
+        print("Final best value: {}".format(self.best_dist))
 
-    # def calculate_distance(self, gene):
-    #     dist = 0
-    #     for i in range(self.sequence_length-1):
-    #         # dist += np.linalg.norm(gene[i]-gene[i+1])
-    #         x = gene[i][0]-gene[i+1][0]
-    #         y = gene[i][1]-gene[i+1][1]
-    #         dist += (x*x + y*y)**0.5
-    #     return dist
+    def calculate_raw_distance(self, gene):
+        """Returns the true distance value of a gene sequence"""
+        dist = 0
+        for i in range(self.sequence_length-1):
+            x = gene[i][0]-gene[i+1][0]
+            y = gene[i][1]-gene[i+1][1]
+            dist += (x*x + y*y)**0.5
+        return dist
 
     def graph(self):
         x = range(len(self.history))
         y = self.history
         plt.plot(x, y)
+        plt.title("Traveling Salesman Genetic Algorithm", loc='center')
         plt.xlabel("Generations")
         plt.ylabel("Best Value")
         plt.show()
 
     def initialize_population(self):
+        """Initializes the starting population"""
         home = self.points_to_visit[self.index_home]
-        gene_sequence = self.points_to_visit.tolist()
-        self.sequence_length = len(gene_sequence)
-        del gene_sequence[self.index_home]
+        temp_gene = self.points_to_visit[0:]
+        self.sequence_length = len(self.points_to_visit)
+        del temp_gene[self.index_home]
 
         for _ in range(self.population_size):
-            gene = np.array(gene_sequence[0:])
-            np.random.shuffle(gene)
-            temp = gene.tolist()
-            gene = np.array([home] + temp)
+            segment = temp_gene[0:]
+            random.shuffle(segment)
+            gene = [home] + segment
             # Record data
             self.population.append(gene)
 
@@ -85,6 +95,7 @@ class TravelingSalesman_GeneticAlgorithm():
         self.best_gene = self.population[best_index]
 
     def compare_best_gene(self):
+        """Compares current_best_dist with all time best_dist and updates values & history"""
         distance = []
         for gene in self.population:
             dist = 0
@@ -134,16 +145,16 @@ class TravelingSalesman_GeneticAlgorithm():
         temp_parents = []
         temp_parents.append(self.best_gene)
 
-        # Locate current best n elites
+        # Locate current best # elites
         for _ in range(self.elite_size):
             # Find index of elite individual
             max_elite_fit = max(fitness)
             elite_index   = fitness.index(max_elite_fit)
             # Clone elite (creates 1 original + 2 mutated per n elites)
-            elite_individual = self.population[elite_index]
-            temp_parents.append(elite_individual)
+            temp_elite = self.population[elite_index]
+            temp_parents.append(temp_elite)
             # Mutation
-            temp_elite = elite_individual.tolist()
+            # temp_elite = elite_individual
             seq_len = len(temp_elite)-1
             while True:
                 mut_0, mut_1 = random.randint(1, seq_len), random.randint(1, seq_len)
@@ -155,11 +166,11 @@ class TravelingSalesman_GeneticAlgorithm():
             gene_bx = temp_elite[mut_1:]
             segment = temp_elite[mut_0:mut_1]
             segment = segment[::-1]
-            temp_parents.append(np.array(gene_ax + segment + gene_bx))
+            temp_parents.append(gene_ax + segment + gene_bx)
             # Shift operator
             shift_gene = temp_elite[1:]
             shift = random.randint(1, len(shift_gene)-1)
-            temp_parents.append(np.array([temp_elite[0]]+shift_gene[shift:]+shift_gene[:shift]))
+            temp_parents.append([temp_elite[0]]+shift_gene[shift:]+shift_gene[:shift])
             # "Remove elite" to find next n elite
             fitness[elite_index] = -999
 
@@ -182,8 +193,6 @@ class TravelingSalesman_GeneticAlgorithm():
         # Clear previous population
         del self.population[0:]
         self.population += temp_parents
-        # print(len(self.population))
-        # print("Average dist", total_dist/self.population_size)
 
     def crossover(self):
         """
@@ -193,8 +202,8 @@ class TravelingSalesman_GeneticAlgorithm():
         queue_indices = [i for i in range(len(self.population)) if random.random() < self.crossover_chance]
         random.shuffle(queue_indices)
         for i in range(0, len(queue_indices)-1, 2):
-            a = self.population[queue_indices[i]].tolist()
-            b = self.population[queue_indices[i+1]].tolist()
+            a = self.population[queue_indices[i]][0:]
+            b = self.population[queue_indices[i+1]][0:]
             seq_len = self.sequence_length-1
             while True:
                 cross_0, cross_1 = random.randint(1, seq_len), random.randint(1, seq_len)
@@ -205,17 +214,17 @@ class TravelingSalesman_GeneticAlgorithm():
             gene_ax = a[0:cross_0]
             gene_ay = a[cross_1:]
             gene_bx = [segment_b for segment_b in b if segment_b not in gene_ax+gene_ay]
-            self.population[i]   = np.array(gene_ax + gene_bx + gene_ay)
+            self.population[i]   = gene_ax+gene_bx+gene_ay
             # # child_b
             gene_bj = b[0:cross_0] 
             gene_bk = b[cross_1:]
             gene_aj = [segment_a for segment_a in a if segment_a not in gene_bj+gene_bk]
-            self.population[i+1] = np.array(gene_bj + gene_aj + gene_bk)
-            
+            self.population[i+1] = gene_bj+gene_aj+gene_bk
+
             # print(a)
-            # print(self.population[i].tolist())
+            # print(self.population[i])
             # print(b)
-            # print(self.population[i+1].tolist())
+            # print(self.population[i+1])
             # print("\n")
 
     def mutate(self):
@@ -224,7 +233,7 @@ class TravelingSalesman_GeneticAlgorithm():
         """
         for i in range(len(self.population)):
             if random.random() < self.mutation_chance:
-                temp_gene = self.population[i].tolist()
+                temp_gene = self.population[i][0:]
                 while True:
                     mut_0, mut_1 = random.randint(1, self.sequence_length), random.randint(1, self.sequence_length)
                     # Mutation at least 2 pieces long (adding +1)
@@ -237,12 +246,12 @@ class TravelingSalesman_GeneticAlgorithm():
                     gene_bx = temp_gene[mut_1:]
                     segment = temp_gene[mut_0:mut_1]
                     segment = segment[::-1]
-                    self.population[i] = np.array(gene_ax + segment + gene_bx)
+                    self.population[i] = gene_ax+segment+gene_bx
                 else:
                     # Shift operator
                     shift_gene = temp_gene[1:]
                     shift = random.randint(1, len(shift_gene)-1)
-                    self.population[i] = np.array([temp_gene[0]] + shift_gene[shift:] + shift_gene[:shift])
+                    self.population[i] = [temp_gene[0]]+shift_gene[shift:]+shift_gene[:shift]
 
 def initalize_binary_ndarray():
     """
@@ -250,7 +259,7 @@ def initalize_binary_ndarray():
     Coordinates is denoted as (row, column)
     For conventional cartesian coordinates (x, y), rotate the array by 90 degress clockwise 
 
-    Returns a nx2 ndarray of all points to visit:
+    Returns a list of all points to visit:
     [[row, column]], [row, column], ...]
     """
     # Test matrix
@@ -275,7 +284,6 @@ def initalize_binary_ndarray():
             if matrix[i, j] == 1:
                 points_to_visit.append([i, j])
     total_points = sum(np.sum(matrix, axis=0))
-    points_to_visit = np.array(points_to_visit)
 
     print("Number to visit: {}".format(total_points))
 
@@ -287,10 +295,11 @@ def starting_point(points_to_visit):
 
     Returns the index of the point that is closest to the origin (0, 0)
     """
-    origin = np.array([0, 0])
-    temp_dist = [np.linalg.norm(points_to_visit[i]-origin) for i in range(len(points_to_visit))]
-    min_dist = min(temp_dist)
-    index_home = temp_dist.index(min_dist)
+    matrix     = np.array(points_to_visit)
+    origin     = np.array([0, 0])
+    distance   = [np.linalg.norm(matrix[i]-origin) for i in range(len(points_to_visit))]
+    min_dist   = min(distance)
+    index_home = distance.index(min_dist)
 
     # print("Starting at {}: {}".format(index_home, points_to_visit[index_home]))
     # print("Distance: {}".format(np.linalg.norm(points_to_visit[index_home]-origin)))
